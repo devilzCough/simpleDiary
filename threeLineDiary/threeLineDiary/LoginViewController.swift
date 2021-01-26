c1a28ee69bdef217244a0e48f21908b57fa46a2a
Jiniz <jiniz.ll@kakao.com>
2021-01-26 16:45:34 +0900

    feat :⚡️ init project
    - git ignore 수정
    - 로그인 화면이 포함된 프로젝트 추가

---
+++ threeLineDiary/threeLineDiary/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  threeLineDiary
+//
+//  Created by 이승진 on 2021/01/26.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
